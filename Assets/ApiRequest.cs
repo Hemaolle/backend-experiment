4c39aa357b94265463eb56ed97977f5f9504923c
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2016-08-26 19:26:38 +0300

    Created a Coroutiner

---
+++ Assets/ApiRequest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApiRequest {
+
+	public static void TestCoroutiner() {
+		Coroutiner.RunCoroutine(new ApiRequest().TestRoutine());
+	}
+
+	public IEnumerator TestRoutine() {		
+		for (int i = 0; i < 5; i++) {
+			Debug.Log("Hello");
+			yield return new WaitForSeconds(1f);
+		}
+	}
+}
---
+++ Assets/Coroutiner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coroutiner : MonoBehaviour {
+
+	static Coroutiner instance;
+
+	void Start()
+	{
+		instance = this;
+	}
+
+	public static void RunCoroutine(IEnumerator coroutine) {
+		instance.StartCoroutine(coroutine);
+	}
+}
